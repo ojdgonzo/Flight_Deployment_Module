1a41b483f26c0391cb9f1904d3407846c5a1189a
ojdgonzo <70494711+ojdgonzo@users.noreply.github.com>
2023-05-23 17:09:16 -0700

    Update app.py
    Added Comments

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
+# importing flask
 from flask import Flask, render_template, request
 
 # importing pickle
 import pickle
 
-
+# Creating our flask project
 app = Flask(__name__)
 app.secret_key = 'jdada921'
 
+# homepage
 @app.route('/')
 def index():
     return render_template('submission.html')
 
-
+# prediction page
+# it will take in any numerical value to see what the possible revenue value
+# using pickle to "unpickle" our file and pull our linear regression prediction 
 @app.route('/prediction', methods = ['POST', 'GET'])
 def prediction():
     delaytime = float(request.form['DelayTime'])
@@ -20,5 +24,6 @@ def prediction():
     # print(prediction)
     return render_template('prediction.html', prediction = int(prediction[0][0]))
 
+# running our project
 if __name__ == '__main__':
     app.run(debug = True)
